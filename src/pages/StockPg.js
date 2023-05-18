b25553f9dee203993fc3817444a0c2578ea471d5
johnr21 <johnrabelo.contact@gmail.com>
2023-05-18 17:41:43 -0300

    addedUseEffectToAPI

--- src/pages/StockPg.js
+++ src/pages/StockPg.js
@@ -1,73 +1,98 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import stockcl from './StockPg.module.css'
 import Plot from 'react-plotly.js';
 
-class Stock extends React.Component {
+function Stock () {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            stockChartXValues: [],
-            stockChartYValues: []
-        }
-    }
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedInfoX, setLoadedInfoX] = useState([]);
+    const [loadedInfoY, setLoadedInfoY] = useState([]);
 
-    componentDidMount () {
-        this.fetchStock();
-    }
+    
+    const API_KEY= 'K80ILXOJ1J111G6I';
+    let StockSymbol = 'MSFT';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
 
-    fetchStock(){
 
-        const pointerToThis = this;
-        
-
-        const API_KEY= 'K80ILXOJ1J111G6I';
-        let StockSymbol = 'MSFT';
-        let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
-        let stockChartXValuesFunction = [];
-        let stockChartYValuesFunction = [];
-
+    useEffect(()=>{
+        setIsLoading(true);
+        {/*fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=compact&apikey=K80ILXOJ1J111G6I')*/}
         fetch(API_Call)
-            .then(
-                function(response){
-                    return response.json();
+        .then(response =>{
+                return response.json();
+            })
+        .then(data =>{
+
+            const auxInfoX = [];
+            const auxInfoY = [];
+
+
+            for (var key in data['Time Series (Daily)']){
+                const infoItemX = {
+                    id: key,
+                    ...data[key]
                 }
-            )
-            .then(
-                function(data){
-                    console.log(data)
 
-                    for( var key in data['Time Series (Daily)']){
-                        stockChartXValuesFunction.push(key);
-                        stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
-                    }
+                auxInfoX.push(key);
 
-                    pointerToThis.setState({
-                        stockChartXValues: stockChartXValuesFunction,
-                        stockChartYValues: stockChartYValuesFunction
-                    })
-
+                const infoItemY = {
+                    id: key,
+                    ...data['Time Series (Daily)'][key]['1. open']
                 }
-            )
-    }
-    render(){
+
+                auxInfoY.push(data['Time Series (Daily)'][key]['1. open']);
+            }
+
+            console.log(auxInfoY);
+            
+            setIsLoading(false);
+            setLoadedInfoX(auxInfoX);
+            setLoadedInfoY(auxInfoY);
+
+            {/*for( var key in data['Time Series (Daily)']){
+                stockChartXValuesFunction.push(key);
+            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']); 
+            }
+
+            pointerToThis.setState({
+                stockChartXValues: stockChartXValuesFunction,
+                stockChartYValues: stockChartYValuesFunction
+            })*/}
+
+            }
+        )           
+    },[])
+
+
+
+
+    if (isLoading){
         return(
-            <div>
-                <Plot
-                    data={[
-                    {
-                        x: this.state.stockChartXValues,
-                        y: this.state.stockChartYValues,
-                        type: 'scatter',
-                        mode: 'lines+markers',
-                        marker: {color: 'red'},
-                    }
-                    ]}
-                    layout={{width: 720, height: 440, title: 'A Fancy Plot'}}
-                />
-            </div>
-        );
+            <section>
+                <p>Loading . . .</p>
+            </section>
+        )
     }
+
+
+
+    return(
+        <div>
+            <Plot
+                data={[
+                {
+                    x: loadedInfoX,
+                    y: loadedInfoY,
+                    type: 'scatter',
+                    mode: 'lines+markers',
+                    marker: {color: 'rgb(16, 132, 77)'},
+                }
+                ]}
+                layout={{width: 720, height: 440, title: 'Stock Price'}}
+            />
+        </div>
+    );
 
 };
 
