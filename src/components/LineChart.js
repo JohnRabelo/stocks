00d86e77fe30206ca1638786335c2941f6c2589a
johnr21 <johnrabelo.contact@gmail.com>
2023-05-21 11:24:47 -0300

    multidimensionalArray

---
+++ src/components/LineChart.js
@@ -0,0 +1,10 @@
+import { Line } from "react-chartjs-2";
+import Chart from 'chart.js/auto'
+
+function LineChart(chartData){
+    return(
+        <Line data={chartData}/>
+    );
+}
+
+export default LineChart
--- src/pages/StockPg.js
+++ src/pages/StockPg.js
@@ -4,48 +4,24 @@ import Plot from 'react-plotly.js';
 
 
 
-function quickSort (arr){
-    if (arr.length < 2){
-        return arr;
-    }
-
-    let pivot = arr[arr.length - 1]
-    let left = [];
-    let right = [];
-    for (let i = 0; i < arr.length - 1; i ++){
-        if (arr[i] < pivot){
-            left.push(arr[i])
-        } else {
-            right.push(arr[i])
-        }
-    }
-    return [...quickSort(left), pivot, ...quickSort(right)]
-}
-
-
 function Stock () {
 
     const [isLoading, setIsLoading] = useState(true);
-    const [loadedInfoX, setLoadedInfoX] = useState([]);
-    const [loadedInfoY, setLoadedInfoY] = useState([]);
-    const arr = [8, 20, -2, 4, -6]
+    let [loadedInfoX, setLoadedInfoX] = useState([]);
+    let [loadedInfoY, setLoadedInfoY] = useState([]);
 
     
     const API_DATA = [
         {
             API_Call: `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=compact&apikey=K80ILXOJ1J111G6I`,
+            Id: 0,
         },
         {
             API_Call: `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMZN&interval=5min&outputsize=compact&apikey=K80ILXOJ1J111G6I`,
+            Id: 1,
         },
-        {
-            API_Call: `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=compact&apikey=K80ILXOJ1J111G6I`,
-        }
     ]
     
-    const API_KEY= 'K80ILXOJ1J111G6I';
-    const counter = [0,1,2]
-
 
     useEffect(()=>{
         setIsLoading(true);
@@ -53,9 +29,6 @@ function Stock () {
         let auxInfoY = [];
         let auxInfoXToPush = new Array();
         let auxInfoYToPush = new Array();
-
-        let countI = 0;
-        let countJ = 0;
 
         API_DATA.map((ItemUnit) => {
 
@@ -106,21 +79,21 @@ function Stock () {
 
     return(
         <div>
-            {counter.map(num => {
+            {API_DATA.map((num) => {
                 console.log(loadedInfoX[0]);
                 console.log(loadedInfoY[0]);
                 return(
                     <Plot
                     data={[
                     {
-                        x: loadedInfoX[num],
-                        y: loadedInfoY[num],
+                        x: loadedInfoX[num.Id],
+                        y: loadedInfoY[num.Id],
                         type: 'scatter',
                         mode: 'lines+markers',
                         marker: {color: 'rgb(16, 132, 77)'},
                     }
                     ]}
-                    layout={{width: 720, height: 440, title: 'Stocks'}}
+                    layout={{width: 720, height: 440, title: num.Id}}
                     />
                 )
             })}
